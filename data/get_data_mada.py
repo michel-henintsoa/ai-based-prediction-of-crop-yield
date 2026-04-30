45a7df29da75e27b1241baaae273a8cea650de63
michel-henintsoa <michelherilalaina@gmail.com>
2026-04-30 11:57:25 +0300

    data

---
+++ data/get_data_mada.py
@@ -0,0 +1,5 @@
+from soilgrids import 
+
+soil_grids = SoilGrids()
+data = soil_grids.get_coverage_data(service_id="phh2o", coverage_id="phh2o_0-5cm_mean", 
+                                    west=47.5, south=-18.8, east=47.6, north=-18.7)
